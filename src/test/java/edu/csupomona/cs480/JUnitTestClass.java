43ab0c23eb391a25c224ac9efdd4e380a83ebed0
Johnny Lu <jhlu@cpp.edu>
2017-10-21 20:05:08 -0700

    added day of the week table

---
+++ src/test/java/edu/csupomona/cs480/JUnitTestClass.java
@@ -0,0 +1,17 @@
+package edu.csupomona.cs480;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// Johnny Lu 
+// Assignment 6
+// Unit Test
+public class JUnitTestClass {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
