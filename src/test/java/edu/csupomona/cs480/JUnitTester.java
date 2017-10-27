5b58b9cee5d787fc97cc14f926733ad76cb60872
Jaeseung <jaeseunglee@cpp.edu>
2017-10-26 17:57:34 -0700

    a

--- src/test/java/edu/csupomona/cs480/JUnitTester.java
+++ src/test/java/edu/csupomona/cs480/JUnitTester.java
@@ -1,11 +1,11 @@
 package edu.csupomona.cs480;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.Assert.*;
 import java.util.Random;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 //Jaeseung Lee 
 //Assignment 6
@@ -14,19 +14,27 @@ class JUnitTester {
 
 	private int value1, value2, result;
 	
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeClass
+	public void setUp() throws Exception {
 		Random r = new Random();
 		value1 = r.nextInt(10)+1;
 		value2 = r.nextInt(10)+1;
 	}
 
 	@Test
-	void testAdd() {
-		result = value1+value2;
-		assertTrue(result == (value1+value2));
+	public void testAdd() {
+		result = add(value1,value2);
 		System.out.println("value1: "+value1);
 		System.out.println("value2: "+value2);
 		System.out.println("result: "+result);
+		
+		if(result== value1+value2)
+		assertTrue(true);
+		else
+			assertTrue(false);
+	}
+	
+	int add(int n1, int n2) {
+		return n1+n2;
 	}
 }
