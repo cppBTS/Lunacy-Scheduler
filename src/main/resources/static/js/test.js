1e253b084dfe2a9626695bed1f6c4eddcbf3e5ce
Justin Han <jdhan@cpp.edu>
2017-10-28 15:40:51 -0700

    Display users in database into frontend

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,4 +1,4 @@
-var cs480App = angular.module('LunacyScheduler', ['ui.bootstrap']);
+var cs480App = angular.module('LunacyScheduler', ['ui.bootstrap', "firebase"]);
 
 var config = {
 	    apiKey: "AIzaSyC28G2x-Xnel2wtmKnw3xi-a8TmWrnJ7tI",
@@ -9,22 +9,30 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-cs480App.controller('DatepickerPopupDemoCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-    $scope.dt.end = new Date();
+cs480App.controller('ReadDataCtrl', function ($scope, $firebaseObject,$firebaseArray) {
+	var userRef = database.ref("user");
+	$scope.users = new $firebaseArray(userRef);
+	
+});
 
-  };
-  $scope.today();
+cs480App.controller('DatepickerPopupDemoCtrl', function ($scope, $log) {
   
   $scope.submit = function() {
-	  firebase.database().ref('test/').set({
+	  database.ref('user/' + $scope.name).set({
 		  	user: $scope.name,
 		  	group: $scope.group,
 		    start: $scope.dt.getMonth() + 1 + "/" + $scope.dt.getDate() + "/" + $scope.dt.getFullYear(),
 		    end: $scope.dt.end.getMonth() + 1 +  "/" + $scope.dt.end.getDate() + "/" + $scope.dt.end.getFullYear()
 		  });
   };
+  
+  //DatePicker
+  $scope.today = function() {
+	    $scope.dt = new Date();
+	    $scope.dt.end = new Date();
+
+  };
+  $scope.today();
 
   $scope.clear = function() {
     $scope.dt = null;
@@ -62,6 +70,84 @@ cs480App.controller('DatepickerPopupDemoCtrl', function ($scope) {
     opened: false
   };
   
+  //TimePicker
+  var d = new Date();
+  d.setHours(0);
+  d.setMinutes(0);
+  $scope.start_sunday = d;
+  $scope.start_monday = d;
+  $scope.start_tuesday = d;
+  $scope.start_wednesday = d;
+  $scope.start_thursday = d;
+  $scope.start_friday = d;
+  $scope.start_saturday= d;
   
+  $scope.end_sunday = d;
+  $scope.end_monday = d;
+  $scope.end_tuesday = d;
+  $scope.end_wednesday = d;
+  $scope.end_thursday = d;
+  $scope.end_friday = d;
+  $scope.end_saturday= d;
+
+  $scope.hstep = 1;
+  $scope.mstep = 1;
+  
+  $scope.ismeridian = true;
+  
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+
+  $scope.startSunday = function () {
+    $log.log('Time changed to: ' + $scope.start_sunday);
+  };
+  $scope.startMonday = function () {
+	$log.log('Time changed to: ' + $scope.start_monday);
+  };
+  $scope.startTuesday = function () {
+	$log.log('Time changed to: ' + $scope.start_tuesday);
+  };
+  $scope.startWednesday = function () {
+    $log.log('Time changed to: ' + $scope.start_wednesday);
+  };
+  $scope.startThursday = function () {
+    $log.log('Time changed to: ' + $scope.start_thursday);
+  };	
+  $scope.startFriday = function () {
+	$log.log('Time changed to: ' + $scope.start_friday);	
+  };
+  $scope.startSaturday = function () {
+	$log.log('Time changed to: ' + $scope.start_saturday);
+  };
+  $scope.endSunday = function () {
+	$log.log('Time changed to: ' + $scope.end_sunday);
+  };
+  $scope.endMonday = function () {
+	$log.log('Time changed to: ' + $scope.end_monday);
+  };
+  $scope.endTuesday = function () {
+	$log.log('Time changed to: ' + $scope.end_tuesday);
+  };
+  $scope.endWednesday = function () {
+	$log.log('Time changed to: ' + $scope.end_wednesday);
+  };
+  $scope.endThursday = function () {
+	$log.log('Time changed to: ' + $scope.end_thursday);
+  };	
+  $scope.endFriday = function () {
+	$log.log('Time changed to: ' + $scope.end_friday);	
+  };
+  $scope.endSaturday = function () {
+    $log.log('Time changed to: ' + $scope.end_saturday);
+};
+			  
+			  
+  
+
+  $scope.clear = function() {
+    $scope.mytime = null;
+  };
 
 });
