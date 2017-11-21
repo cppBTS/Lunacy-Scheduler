369808aaadfb8ca9b4a3c2a232ee97a9d328be61
Ubaldo Jimenez Prieto <ubaldoj@cpp.edu>
2017-11-21 02:18:37 -0800

    Added Users Table on Event Page

--- src/main/resources/static/js/event_description.js
+++ src/main/resources/static/js/event_description.js
@@ -40,4 +40,44 @@ cs480App.controller('eventDescription', function ($scope, $firebaseArray,$fireba
           $scope.users = event.users;
       });
 
+			//Table Filter function
+			(function(){
+			  'use strict';
+			  var $ = jQuery;
+			  $.fn.extend({
+			    filterTable: function(){
+			      return this.each(function(){
+			        $(this).on('keyup', function(e){
+			          var $this = $(this), search = $this.val().toLowerCase(), target = $this.attr('data-filters'), $rows = $(target).find('tbody tr');
+			          if(search == '') {
+			            $rows.show();
+			          } else {
+			            $rows.each(function(){
+			              var $this = $(this);
+			              $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+			            })
+			          }
+			        });
+			      });
+			    }
+			  });
+			  $('[data-action="filter"]').filterTable();
+			})(jQuery);
+
+			$(function(){
+			    // attach table filter plugin to inputs
+			  $('[data-action="filter"]').filterTable();
+
+			  $('.container').on('click', '.panel-heading span.filter', function(e){
+			    var $this = $(this),
+			        $panel = $this.parents('.panel');
+
+			    $panel.find('.panel-body').slideToggle();
+			    if($this.css('display') != 'none') {
+			      $panel.find('.panel-body input').focus();
+			    }
+			  });
+			  $('[data-toggle="tooltip"]').tooltip();
+			})
+
 });
